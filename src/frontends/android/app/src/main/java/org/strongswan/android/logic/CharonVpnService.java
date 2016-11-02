03c91b1e03b5751f46e25de2f75849884d6dbc43
Tobias Brunner <tobias@strongswan.org>
2016-11-02 18:26:43 +0100

    wip: android: Display a permanent notification while connected
    This forces the service to run in the foreground, meaning the system
    won't kill it when low on memory.
    
    WIP: maybe add a public notification (state but not the profile name)

--- src/frontends/android/app/src/main/java/org/strongswan/android/logic/CharonVpnService.java
+++ src/frontends/android/app/src/main/java/org/strongswan/android/logic/CharonVpnService.java
@@ -17,29 +17,9 @@
 
 package org.strongswan.android.logic;
 
-import java.io.File;
-import java.net.Inet4Address;
-import java.net.Inet6Address;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.security.PrivateKey;
-import java.security.cert.CertificateEncodingException;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
-import org.strongswan.android.data.VpnProfile;
-import org.strongswan.android.data.VpnProfileDataSource;
-import org.strongswan.android.data.VpnType.VpnTypeFeature;
-import org.strongswan.android.logic.VpnStateService.ErrorState;
-import org.strongswan.android.logic.VpnStateService.State;
-import org.strongswan.android.logic.imc.ImcState;
-import org.strongswan.android.logic.imc.RemediationInstruction;
-import org.strongswan.android.ui.MainActivity;
-import org.strongswan.android.utils.SettingsWriter;
-
 import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ComponentName;
@@ -53,13 +33,39 @@ import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
 import android.security.KeyChain;
 import android.security.KeyChainException;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.system.OsConstants;
 import android.util.Log;
+
+import org.strongswan.android.R;
+import org.strongswan.android.data.VpnProfile;
+import org.strongswan.android.data.VpnProfileDataSource;
+import org.strongswan.android.data.VpnType.VpnTypeFeature;
+import org.strongswan.android.logic.VpnStateService.ErrorState;
+import org.strongswan.android.logic.VpnStateService.State;
+import org.strongswan.android.logic.imc.ImcState;
+import org.strongswan.android.logic.imc.RemediationInstruction;
+import org.strongswan.android.ui.MainActivity;
+import org.strongswan.android.utils.SettingsWriter;
+
+import java.io.File;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.PrivateKey;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class CharonVpnService extends VpnService implements Runnable
 {
 	private static final String TAG = CharonVpnService.class.getSimpleName();
 	public static final String LOG_FILE = "charon.log";
+	public static final int VPN_STATE_NOTIFICATION_ID = 1;
 
 	private String mLogFile;
 	private VpnProfileDataSource mDataSource;
@@ -223,6 +229,7 @@ public class CharonVpnService extends VpnService implements Runnable
 						BuilderAdapter builder = new BuilderAdapter(mCurrentProfile.getName(), mCurrentProfile.getSplitTunneling());
 						if (initializeCharon(builder, mLogFile, mCurrentProfile.getVpnType().has(VpnTypeFeature.BYOD)))
 						{
+							addNotification();
 							Log.i(TAG, "charon started");
 							SettingsWriter writer = new SettingsWriter();
 							writer.setValue("global.language", Locale.getDefault().getLanguage());
@@ -268,8 +275,75 @@ public class CharonVpnService extends VpnService implements Runnable
 				deinitializeCharon();
 				Log.i(TAG, "charon stopped");
 				mCurrentProfile = null;
+				removeNotification();
 			}
 		}
+	}
+
+	/**
+	 * Add a permanent notification while we are connected to avoid the service getting killed by
+	 * the system when low on memory.
+	 */
+	private void addNotification()
+	{
+		startForeground(VPN_STATE_NOTIFICATION_ID, buildNotification(State.CONNECTING));
+	}
+
+	/**
+	 * Update the currently shown notification.
+	 */
+	private void updateNotification(State state)
+	{
+		synchronized (this) {
+			if (mCurrentProfile != null) {
+				NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+				manager.notify(VPN_STATE_NOTIFICATION_ID, buildNotification(state));
+			}
+		}
+	}
+
+	/**
+	 * Build a notification matching the given state
+	 *
+	 * @param state current state
+	 */
+	private Notification buildNotification(State state)
+	{
+		android.support.v4.app.NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+				.setContentText(mCurrentProfile.getName())
+				.setSmallIcon(R.drawable.ic_notification)
+				.setCategory(NotificationCompat.CATEGORY_SERVICE)
+				.setVisibility(NotificationCompat.VISIBILITY_SECRET);
+		int s = R.string.state_disabled;
+		switch (state) {
+			case CONNECTING:
+				s = R.string.state_connecting;
+				builder.setColor(ContextCompat.getColor(this, R.color.warning_text));
+				break;
+			case CONNECTED:
+				s = R.string.state_connected;
+				builder.setColor(ContextCompat.getColor(this, R.color.success_text));
+				builder.setUsesChronometer(true);
+				break;
+			case DISCONNECTING:
+				s = R.string.state_disconnecting;
+				break;
+		}
+		builder.setContentTitle(getString(s));
+
+		Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+		PendingIntent pending = PendingIntent.getActivity(getApplicationContext(), 0, intent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		builder.setContentIntent(pending);
+		return builder.build();
+	}
+
+	/**
+	 * Remove the permanent notification.
+	 */
+	private void removeNotification()
+	{
+		stopForeground(true);
 	}
 
 	/**
@@ -374,10 +448,12 @@ public class CharonVpnService extends VpnService implements Runnable
 				if (!mIsDisconnecting)
 				{
 					setState(State.CONNECTING);
+					updateNotification(State.CONNECTING);
 				}
 				break;
 			case STATE_CHILD_SA_UP:
 				setState(State.CONNECTED);
+				updateNotification(State.CONNECTED);
 				break;
 			case STATE_AUTH_ERROR:
 				setErrorDisconnect(ErrorState.AUTH_FAILED);
